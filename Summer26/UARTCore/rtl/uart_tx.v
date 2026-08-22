216cc9c0f0c2d7a1f83c5ed44b7d236acc18cdb8
areur <bmafeilx824@gmail.com>
2026-08-22 00:33:45 -0400

    started project 10, i think its broken tho

---
+++ Summer26/UARTCore/rtl/uart_tx.v
@@ -0,0 +1,110 @@
+`timescale 1ns / 1ps
+
+module uart_tx #(
+    parameter BAUD_RATE = 9_600,
+    parameter BITS_PER_WORD = 8,
+    parameter PARITY = 2, //0 - Off, 1 - Odd Parity, 2 - Even Parity
+    parameter NUM_STOP_BITS = 2
+  ) (
+    input wire clk, //General system clock
+    input wire rst,
+    input wire tx_tick, //Clock Enable for when the Baud Generator ticks the system
+    input wire [BITS_PER_WORD-1:0] in_parallel_Data, //Data to be transmitted
+    input wire TX_VALID, //HIGH when TX has new valid data to transmit
+    output reg TX_ACTIVE, //HIGH when TX is working
+    output reg out_dataTX //This is the single line of the data being sent out
+    
+  );
+
+  //STATES
+  localparam IDLE = 0;
+  localparam START_BIT = 1;
+  localparam SENDING_DATA_BITS = 2;
+  localparam PARITY_BIT = 3;
+  localparam STOP_BITS = 4;
+
+  //TIMING
+  //localparam CLKS_PER_BIT = (CLOCK_RATE / (BAUD_RATE)) - 1;
+  reg [3:0] stateCounter = 3'b0;
+
+  //STATE MACHINE VARS
+  reg [2:0] currentState = IDLE;
+
+  reg [BITS_PER_WORD-1:0] currentData_latch;
+
+  always @(posedge clk)
+  begin: stateMachine
+    if (rst)
+    begin
+      TX_ACTIVE <= 0;
+    end
+    else if (tx_tick)
+    begin
+      case (currentState)
+        IDLE:
+        begin
+          if (TX_VALID) begin
+            currentState <= START_BIT;
+            TX_ACTIVE <= 1'b1;
+
+            //latch data to work on right now
+            currentData_latch <= in_parallel_Data;
+          end
+          else begin
+            TX_ACTIVE <= 0;
+            stateCounter <= 0;
+            out_dataTX <= 1;
+          end
+        end
+        START_BIT:
+        begin
+          //start the start bit and continue to next state
+          out_dataTX <= 0;
+          currentState <= SENDING_DATA_BITS;
+        end
+        SENDING_DATA_BITS: //NOT FIXED (NOT 1 CYCLE)
+        begin
+          //Left-shift a piece of data out at the next tick
+          currentData_latch <= {currentData_latch[BITS_PER_WORD-2:0],1'b0};
+          //Output the piece of data that's getting shifted out
+          out_dataTX <= currentData_latch[BITS_PER_WORD-1];
+
+          //keep track of how many bits have been sent out
+          if (stateCounter == BITS_PER_WORD-1) begin
+            //done transmitting, move on to next state
+            currentState <= PARITY ? PARITY_BIT : STOP_BITS;
+            stateCounter <= 0;
+          end
+          else begin
+            stateCounter <= stateCounter + 3'b1;
+          end
+        end
+        PARITY_BIT: //NOT FIXED (NOT 1 CYCLE)
+        begin
+          out_dataTX <= (PARITY == 2) ? ^currentData_latch : ~^currentData_latch;
+          //If true: Even parity (first value), if false: Odd parity (second value)
+
+          stateCounter <= 0;
+          currentState <= STOP_BITS;
+        end
+        STOP_BITS: // NOT FIXED (NOT 1 CYCLE)
+        begin
+          if (stateCounter == NUM_STOP_BITS - 1) begin
+            //end stop bits
+            stateCounter <= 0;
+
+            TX_ACTIVE <= 0;
+            currentState <= IDLE;
+            currentData_latch <= 0;
+          end
+          else begin
+            //start or continue stop bits
+            out_dataTX <= 1;
+            stateCounter <= stateCounter + 1;
+          end
+        end
+        endcase
+    end
+  end
+
+endmodule
---
+++ Summer26/UARTCore/tb/uart_tx_tb.v
@@ -0,0 +1,74 @@
+`timescale 1ns / 1ps
+
+module uart_tx_tb();
+
+  localparam BITS_PER_WORD = 8;
+
+  reg tclk, trst, t_tx_tick, t_tx_valid;
+  reg [BITS_PER_WORD-1:0] tx_input_data;
+  wire t_tx_on, tx_output_data;
+  uart_tx #(
+            .BAUD_RATE(9_600),
+            .BITS_PER_WORD(BITS_PER_WORD),
+            .PARITY(2), //0 - Off, 1 - Odd Parity, 2 - Even Parity
+            .NUM_STOP_BITS(2)
+          ) DUT (
+            .clk(tclk), //General system clock
+            .rst(trst),
+            .tx_tick(t_tx_tick), //Clock Enable for when the Baud Generator ticks the system
+            .in_parallel_Data(tx_input_data), //Data to be transmitted
+            .TX_VALID(t_tx_valid), //HIGH when TX has new valid data to transmit
+            .TX_ACTIVE(t_tx_on), //HIGH when TX is working
+            .out_dataTX(tx_output_data) //This is the single line of the data being sent out
+          );
+
+  //timing
+  reg [2:0] tickCounter = 3'b0;
+  always #1
+  begin
+    if (trst)
+    begin
+      tclk = 1'b0;
+      tickCounter = 3'b0;
+      t_tx_tick = 1'b0;
+    end
+    else
+    begin
+      tclk = ~tclk;
+      tickCounter = tickCounter + 3'b1;
+      t_tx_tick = 1'b0;
+      if (tickCounter >= 3'd5)
+      begin
+        t_tx_tick = 1'b1;
+        tickCounter = 3'b0;
+      end
+      else
+      begin
+        t_tx_tick = 1'b0;
+      end
+    end
+  end
+
+  integer i;
+
+  initial
+  begin
+    $dumpfile("sim/sim.vcd");          // Specifies the VCD file path
+    $dumpvars(0, uart_tx_tb); // Dumps all signals in this module and submodules
+    tclk = 1'b0;
+    trst = 1'b1;
+    #5;
+    trst = 1'b0;
+
+    for (i=0; i<5; i=i+1)
+    begin
+      tx_input_data = $urandom & {BITS_PER_WORD{1'd1}};
+      t_tx_valid = 1'b1;
+      #5
+      t_tx_valid = 1'b0;
+      #245;
+    end
+    $finish;
+  end
+
+endmodule
