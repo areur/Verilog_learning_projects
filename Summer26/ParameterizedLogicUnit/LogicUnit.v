370fadd5448b1ff7a9aaf541e08b72ec03df17b6
areur <bmafeilx824@gmail.com>
2026-08-21 23:46:02 -0400

    tested Verilog formatter

--- Summer26/ParameterizedLogicUnit/LogicUnit.v
+++ Summer26/ParameterizedLogicUnit/LogicUnit.v
@@ -1,40 +1,47 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 06/17/2026 07:38:02 PM
-// Design Name: 
+// Design Name:
 // Module Name: LogicUnit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module LogicUnit #(parameter BUS_WIDTH = 32)(
-    input [(BUS_WIDTH-1):0] a, 
-    input [(BUS_WIDTH-1):0] b, 
+    input [(BUS_WIDTH-1):0] a,
+    input [(BUS_WIDTH-1):0] b,
     input [2:0] sel,
     output reg [(BUS_WIDTH-1):0] y
-);
-    
-    always @(*) begin
-        case (sel)
-            3'd0: y = a&b; //AND
-            3'd1: y = a|b; //OR
-            3'd2: y = a^b; //XOR
-            3'd3: y = ~(a&b); //NAND
-            3'd4: y = ~(a|b); //NOR
-            default: y = {BUS_WIDTH{1'd0}}; //zero, ext operation { will extend it to match the bus width
-        endcase
-    end
+  );
+
+  always @(*)
+  begin
+    case (sel)
+      3'd0:
+        y = a&b; //AND
+      3'd1:
+        y = a|b; //OR
+      3'd2:
+        y = a^b; //XOR
+      3'd3:
+        y = ~(a&b); //NAND
+      3'd4:
+        y = ~(a|b); //NOR
+      default:
+        y = {BUS_WIDTH{1'd0}}; //zero, ext operation { will extend it to match the bus width
+    endcase
+  end
 endmodule
